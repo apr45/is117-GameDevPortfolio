6770a1a6134c8905b4c4bbbba5ce113a99182bb1
Alex <apr45@njit.edu>
2026-04-26 14:51:06 -0400

    intergrating necessary code to import css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './styles/index.css' // Global styles (like your 8-bit variables)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
